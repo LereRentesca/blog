d6533b5d93a87ce19a2f54edb01a08a8db1c1f46
LereRentesca <esteban.renteria@uabc.edu.mx>
2023-05-03 20:04:32 -0700

    Assignment 04

---
+++ posts/views.py
@@ -0,0 +1,33 @@
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["author","title","subtitle","body"]
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title","subtitle","body"]
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("post_list")
